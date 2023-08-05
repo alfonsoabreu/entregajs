885e78be57b72727f2e527be4b76f358978a2564
alfonsoabreu <134788188+alfonsoabreu@users.noreply.github.com>
2023-08-06 00:25:16 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let afili = prompt("Esta registrado, ingrese si o no.").toLocaleUpperCase();
+let libf = 50;
+let liba = 50;
+let libc = 100;
+
+if (afili = "si"){
+    let NlibF = parseint(prompt("cantidad de libros de ficcion "));
+    let NlibA = parseint(prompt("cantidad de libros de accion"));
+    let NlibC = packagerseint(prompt("cantidad de libros comedia"));
+    const cuenta = (`El monto total es: ${libf*Nlibrosficcion} + ${liba*Nlibrosaccion} + ${libc*Nlibroscomedia}`)
+}else{
+    alert("debe registrarse antes de ingresar");
+}
